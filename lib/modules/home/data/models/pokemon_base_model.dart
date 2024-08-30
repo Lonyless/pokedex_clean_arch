43278e60b0ae89acbc5b2bba1a524f523dcae6b7
Victor Batista <48859696+Lonyless@users.noreply.github.com>
2024-08-29 22:41:16 -0300

    criando model base da lista de pokemons

---
+++ lib/modules/home/data/models/pokemon_base_model.dart
@@ -0,0 +1,15 @@
+import 'package:pokedex_clean_arch/modules/home/domain/entities/pokemon_base_entity.dart';
+
+class PokemonBaseModel extends PokemonBaseEntity {
+  PokemonBaseModel({
+    required super.name,
+    required super.url,
+  });
+
+  factory PokemonBaseModel.fromMap(Map<String, dynamic> data) {
+    return PokemonBaseModel(
+      name: data['name'],
+      url: data['url'],
+    );
+  }
+}
