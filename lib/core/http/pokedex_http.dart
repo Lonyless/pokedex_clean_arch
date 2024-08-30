5a0896f732dda0f80d902f7095c19077d8dbbd91
Victor Batista <48859696+Lonyless@users.noreply.github.com>
2024-08-29 22:40:38 -0300

    criando http

--- lib/core/http/pokedex_http.dart
+++ lib/core/http/pokedex_http.dart
@@ -1,10 +1,25 @@
-
-
 import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
 
-@LazySingleton
+@singleton
 class PokedexHttp {
   final Dio dio;
-  
+
   const PokedexHttp(this.dio);
+
+  Future<Response> post() async {
+    throw UnimplementedError();
+  }
+
+  Future<Response> get(String path, Map<String, dynamic> queryParameteres) async {
+    return await dio.get(path, queryParameters: queryParameteres);
+  }
+
+  Future<Response> put() async {
+    throw UnimplementedError();
+  }
+
+  Future<Response> delete() async {
+    throw UnimplementedError();
+  }
 }
