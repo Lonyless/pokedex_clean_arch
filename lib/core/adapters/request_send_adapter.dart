4acb498a9b3c6e9241fb1ee34b3a0b4da50fb580
Victor Batista <48859696+Lonyless@users.noreply.github.com>
2024-08-29 22:40:18 -0300

    criando adapter para tratar erros no dio

---
+++ lib/core/adapters/request_send_adapter.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:pokedex_clean_arch/core/errors/server_exception.dart';
+
+class RequestSendAdapter {
+  static Future<T> guard<T>(Future<T> Function() callback) async {
+    try {
+      return await callback();
+    } on DioException catch (e) {
+      String message = "Erro ao se conectar ao servidor";
+
+      if (e.response != null) {
+        if (e.response!.statusCode == 404) {
+          message = "Não encontrado";
+        }
+        if (e.response!.statusCode == 500) {
+          message = "Falha geral no servidor";
+        }
+      }
+
+      throw ServerException(message);
+    }
+  }
+}
