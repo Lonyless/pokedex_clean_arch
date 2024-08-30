adf3e5ef3469d216f8e681c3a71f58ff515a603f
Victor Batista <48859696+Lonyless@users.noreply.github.com>
2024-08-30 00:06:53 -0300

    criando cards para os pokemons

--- lib/modules/home/presentation/home_page.dart
+++ lib/modules/home/presentation/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'dart:math';
+import 'package:pokedex_clean_arch/core/enum/api_request_state.dart';
+
 import 'package:pokedex_clean_arch/core/get_it.dart';
 import 'package:pokedex_clean_arch/modules/home/presentation/store/home_store.dart';
 import 'package:pokedex_clean_arch/modules/home/presentation/widgets/search_button.dart';
@@ -12,6 +16,14 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var homeStore = getIt.get<HomeStore>();
+
+  final colorList = [
+    Color(0xff68A890),
+    Color(0xff498899),
+    Color(0xffD7705F),
+    Color(0xff9FB0D1),
+    Color(0xff70B8C7),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -30,8 +42,62 @@ class _HomePageState extends State<HomePage> {
         elevation: 22,
         shadowColor: Colors.black,
       ),
-      body: Column(
-        children: [],
+      body: Observer(
+        builder: (context) {
+          if (homeStore.apiRequestState == ApiRequestState.loading) {
+            return CircularProgressIndicator();
+          }
+
+          if (homeStore.apiRequestState == ApiRequestState.done) {
+            return SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 16),
+                    Wrap(
+                      children: [
+                        for (final pokemon in homeStore.pokemonBaseList)
+                          Builder(
+                            builder: (context) {
+                              final color = colorList[Random().nextInt(4)].withOpacity(0.2);
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                                  decoration: BoxDecoration(
+                                    color: color,
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      Image.network(
+                                        "https://img.pokemondb.net/artwork/${pokemon.name}.jpg",
+                                        width: MediaQuery.of(context).size.width * 0.2,
+                                        height: MediaQuery.of(context).size.width * 0.2,
+                                        colorBlendMode: BlendMode.multiply,
+                                        color: color,
+                                      ),
+                                      const SizedBox(height: 8),
+                                      Text(
+                                        pokemon.name,
+                                        style: TextStyle(fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
+
+          return SizedBox();
+        },
       ),
     );
   }
