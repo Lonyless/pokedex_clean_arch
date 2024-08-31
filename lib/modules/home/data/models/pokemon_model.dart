142d0495fb173a5a33927b31d940490a405020f1
Victor Batista <48859696+Lonyless@users.noreply.github.com>
2024-08-31 11:07:44 -0300

    modelando dados do pokemon

---
+++ lib/modules/home/data/models/pokemon_model.dart
@@ -0,0 +1,13 @@
+import 'package:pokedex_clean_arch/modules/home/domain/entities/pokemon_entity.dart';
+
+class PokemonModel extends PokemonEntity {
+  PokemonModel({required super.hp, required super.attack, required super.defense});
+
+  factory PokemonModel.fromMap(Map<String, dynamic> data) {
+    return PokemonModel(
+      hp: data['stats'][0]['base_stat'],
+      attack: data['stats'][1]['base_stat'],
+      defense: data['stats'][2]['base_stat'],
+    );
+  }
+}
---
+++ lib/modules/home/domain/entities/pokemon_entity.dart
@@ -0,0 +1,7 @@
+class PokemonEntity {
+  final double hp;
+  final double attack;
+  final double defense;
+
+  PokemonEntity({required this.hp, required this.attack, required this.defense});
+}
