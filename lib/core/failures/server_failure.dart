26439a0eae67a2a6a182f27cd6e1607d27123ca5
Victor Batista <48859696+Lonyless@users.noreply.github.com>
2024-08-31 12:23:14 -0300

    movendo construtor para failure

--- lib/core/failures/server_failure.dart
+++ lib/core/failures/server_failure.dart
@@ -3,8 +3,4 @@ import 'package:pokedex_clean_arch/core/failures/failure.dart';
 
 class ServerFailure extends Failure {
   ServerFailure(super.message);
-
-  factory ServerFailure.fromException(ServerException e) {
-    return ServerFailure(e.message);
-  }
 }
